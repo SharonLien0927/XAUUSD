2b1ca071af5ae3dfcced24eefcf06c82fb388b3e
SharonLien0927 <sharon1062001@gmail.com>
2025-12-26 15:06:09 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/XAUUSD/',  // 改為你的倉庫名稱
+  base: '/XAUUSD/',
   plugins: [vue()],
 })
